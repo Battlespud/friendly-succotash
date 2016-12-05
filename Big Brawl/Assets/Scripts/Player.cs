5ab20cfcff3ab74013c75a32a6b382447e154434
xnenaixyinaa <rxjohnston@gmail.com>
2016-12-05 00:14:29 -0900

    Initial Commit

---
+++ Big Brawl/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void movePlayer() {
+		rotatePlayer ();
+		transform.Translate (PlayerControls.movePlayer ());
+	}
+
+	void rotatePlayer(){
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey (KeyCode.RightArrow)) {
+				transform.Rotate (0, 45, 0);
+			} else if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey (KeyCode.LeftArrow)) {
+				transform.Rotate (0, 315, 0);
+			} else {
+				transform.Rotate (0, 0, 0);
+			}
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey (KeyCode.RightArrow)) {
+				transform.Rotate (0, 135, 0);
+			} else if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey (KeyCode.LeftArrow)) {
+				transform.Rotate (0, 225, 0);
+			} else {
+				transform.Rotate (0, 180, 0);
+			}
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (0, 90, 0);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (0, 270, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movePlayer ();
+	}
+}
---
+++ Big Brawl/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerControls {
+	const float moveSpeed = 4f;
+	//x(+R/-L),y(+U/-D),z(+F/-B)
+	//Input.GetKey (KeyCode.UpArrow)
+	//Input.GetKey (KeyCode.DownArrow)
+	//Input.GetKey(KeyCode.RightArrow)
+	//Input.GetKey(KeyCode.LeftArrow)
+	public static Vector3 movePlayer(){
+		
+		Vector3 position = new Vector3 (0, 0, 0);
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey (KeyCode.RightArrow)) {
+				position += new Vector3 (1, 0, 1)*2;
+				return (position * moveSpeed) * Time.deltaTime;
+			} else if (Input.GetKey (KeyCode.UpArrow) && Input.GetKey (KeyCode.LeftArrow)) {
+				position += new Vector3 (-1, 0, 1)*2;
+				return (position * moveSpeed) * Time.deltaTime;
+			} else {
+				position += new Vector3 (0, 0, 1);
+				return ((position * moveSpeed) * Time.deltaTime)*2;
+			}
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey (KeyCode.RightArrow)) {
+				position += new Vector3 (1, 0, -1)*2;
+				return (position * moveSpeed) * Time.deltaTime;
+			} else if (Input.GetKey (KeyCode.DownArrow) && Input.GetKey (KeyCode.LeftArrow)) {
+				position += new Vector3 (-1, 0, -1)*2;
+				return (position * moveSpeed) * Time.deltaTime;
+			} else {
+				position += new Vector3 (0, 0, -1);
+				return ((position * moveSpeed) * Time.deltaTime)*2;
+			}
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			position += new Vector3 (1, 0, 0);
+			return ((position * moveSpeed) * Time.deltaTime)*2;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			position += new Vector3 (-1, 0, 0);
+			return ((position * moveSpeed) * Time.deltaTime)*2;
+		}
+
+		return position * moveSpeed;
+	}
+}
